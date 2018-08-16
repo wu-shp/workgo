adf10673e1f1afa5530f431d648e2251558f3249
wusp <15524101372@163.com>
2018-08-16 10:19:52 +0800

    代码提交

---
+++ sorter.go
@@ -0,0 +1,56 @@
+package main
+
+import "bufio"
+import "flag"
+import "fmt"
+import "io"
+import "os"
+import "strconv"
+
+var infile *string = flag.String("i", "unsorted.dat", "File contains values for sorting")
+var outfile *string = flag.String("o", "sorted.dat", "File to receive sorted values")
+var algorithm *string = flag.String("a", "qsort", "Sort algorithm")
+
+func readValues(infile string) (values []int, err error) {
+	file, err := os.Open(infile)
+	if err != nil {
+		fmt.Println("Failed to open the input file ", infile)
+		return
+	}
+	defer file.Close()
+	br := bufio.NewReader(file)
+	values = make([]int, 0)
+	for {
+		line, isPrefix, err1 := br.ReadLine()
+		if err1 != nil {
+			if err1 != io.EOF {
+				err = err1
+			}
+			break
+		}
+		if isPrefix {
+			fmt.Println("A too long line, seems unexpected.")
+			return
+		}
+		str := string(line) // 转换字符数组为字符串
+		value, err1 := strconv.Atoi(str)
+		if err1 != nil {
+			err = err1
+			return
+		}
+		values = append(values, value)
+	}
+	return
+}
+func main() {
+	flag.Parse()
+	if infile != nil {
+		fmt.Println("infile =", *infile, "outfile =", *outfile, "algorithm =", *algorithm)
+	}
+	values, err := readValues(*infile)
+	if err == nil {
+		fmt.Println("Read values:", values)
+	} else {
+		fmt.Println(err)
+	}
+}
